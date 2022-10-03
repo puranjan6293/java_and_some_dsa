89e7344d57e1d2678fdce2345c1a8a78ad27399e
puranjan <mallik.puranjan@gmail.com>
2022-10-03 16:58:49 +0530

    added new question with solution code

---
+++ src/Gfg_Problems_Solutions_Java/Find_all_pairs_with_given_um_java.java
@@ -0,0 +1,21 @@
+package Gfg_Problems_Solutions_Java;
+import java.util.*;
+
+public class Find_all_pairs_with_given_um_java {
+    public pair[] allPairs( long A[], long B[], long N, long M, long X) {
+        // Your code goes here
+        ArrayList<pair> lis = new ArrayList<>();
+        HashSet<Long> hs = new HashSet<>();
+        for(long i :B){
+            hs.add(i);
+        }
+        Arrays.sort(A);
+        for(long i:A){
+            if(hs.contains(X-i)){
+                lis.add(new pair(i,X-i));
+            }
+        }
+        pair[] list = new pair[lis.size()];
+        return lis.toArray(list);
+    }
+}
