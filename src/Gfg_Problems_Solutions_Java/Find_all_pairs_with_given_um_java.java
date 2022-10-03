d7737502000159d2f2e7062a16a65d994882d8bf
puranjan <mallik.puranjan@gmail.com>
2022-10-03 16:59:34 +0530

    added new question with solution code

--- src/Gfg_Problems_Solutions_Java/Find_all_pairs_with_given_um_java.java
+++ src/Gfg_Problems_Solutions_Java/Find_all_pairs_with_given_um_java.java
@@ -2,6 +2,7 @@ package Gfg_Problems_Solutions_Java;
 import java.util.*;
 
 public class Find_all_pairs_with_given_um_java {
+    /*
     public pair[] allPairs( long A[], long B[], long N, long M, long X) {
         // Your code goes here
         ArrayList<pair> lis = new ArrayList<>();
@@ -18,4 +19,5 @@ public class Find_all_pairs_with_given_um_java {
         pair[] list = new pair[lis.size()];
         return lis.toArray(list);
     }
+     */
 }
