407445ca7a8592548862f483d98f28bf4a692ce5
puranjan <mallik.puranjan@gmail.com>
2022-10-04 18:46:49 +0530

    new things added

--- src/DSA_Problems/Find_First_and_last_Occurence_Recursion.java
+++ src/DSA_Problems/Find_First_and_last_Occurence_Recursion.java
@@ -1,14 +1,17 @@
 package DSA_Problems;
 //Find the first and last occurrence of a element in a string, Time complexity --> O(n)
 public class Find_First_and_last_Occurence_Recursion {
+    //step1
     public static int first = -1;
     public static int last = -1;
     public static void firstLastIndex(String str, int idx, char element){
+        //step3
         if(idx==str.length()){
             System.out.println(first);
             System.out.println(last);
             return;
         }
+        //step2
         char current = str.charAt(idx);
         if(current==element){
             if(first==-1){
