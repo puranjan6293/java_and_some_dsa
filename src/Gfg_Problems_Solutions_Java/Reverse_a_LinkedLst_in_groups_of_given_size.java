5e08a3a3c7cac39a5716304fabdb3b7bc6f5f552
puranjan <mallik.puranjan@gmail.com>
2022-10-05 16:21:09 +0530

    new things added

---
+++ src/Gfg_Problems_Solutions_Java/Reverse_a_LinkedLst_in_groups_of_given_size.java
@@ -0,0 +1,28 @@
+package Gfg_Problems_Solutions_Java;
+
+public class Reverse_a_LinkedLst_in_groups_of_given_size {
+    /*
+    public static Node reverse(Node node, int k)
+    {
+        //Your code here
+        Node curr = node;
+        Node prev = null;
+        Node next = null;
+
+        int count =0;
+        while(count<k && curr!=null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+
+            count++;
+        }
+        if(next!=null){
+            node.next = reverse(next,k);
+        }
+        return prev;
+    }
+
+     */
+}
