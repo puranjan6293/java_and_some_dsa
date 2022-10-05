5838f51ba380adbd162d130c2feccaf72a10f439
puranjan <mallik.puranjan@gmail.com>
2022-10-05 14:33:49 +0530

    new things added

---
+++ src/Gfg_Problems_Solutions_Java/Pairwise_swap_elements_of_a_linked_ist_linkedList.java
@@ -0,0 +1,22 @@
+package Gfg_Problems_Solutions_Java;
+
+public class Pairwise_swap_elements_of_a_linked_ist_linkedList {
+    /*
+    public Node pairwiseSwap(Node head)
+    {
+        // code here
+        if(head==null || head.next==null){
+            return head;
+        }
+        Node temp = head.next.next;
+        Node first = head.next;
+        Node second = head;
+
+        head=first;
+        head.next=second;
+        second.next = pairwiseSwap(temp);
+
+        return first;
+    }
+     */
+}
