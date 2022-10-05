a8d895d88ada06b0dce4d3c6ae16c04bec133131
puranjan <mallik.puranjan@gmail.com>
2022-10-05 15:06:03 +0530

    new things added

---
+++ src/Gfg_Problems_Solutions_Java/Remove_Loop_in_a_linked_list.java
@@ -0,0 +1,20 @@
+package Gfg_Problems_Solutions_Java;
+
+public class Remove_Loop_in_a_linked_list {
+    /*
+    //Function to remove a loop in the linked list.
+    public static void removeLoop(Node head){
+        // code here
+        // remove the loop without losing any nodes
+        //using HashSet
+        HashSet<Node> hs = new HashSet<>();
+        hs.add(head);
+        while(hs.contains(head.next)==false && head.next!=null){
+            hs.add(head.next);
+            head=head.next;
+        }
+        head.next=null;
+    }
+
+     */
+}
