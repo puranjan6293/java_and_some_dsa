b6ef5f3c96a8d5beae61c44f027ff77fe1cce35e
puranjan <mallik.puranjan@gmail.com>
2022-10-05 19:24:19 +0530

    new things added

---
+++ src/Gfg_Problems_Solutions_Java/Find_middle_element_in_a_LinkedList.java
@@ -0,0 +1,18 @@
+package Gfg_Problems_Solutions_Java;
+
+public class Find_middle_element_in_a_LinkedList {
+    /*
+    int getMiddle(Node head)
+    {
+        // Your code here.
+        Node slow = head;
+        Node fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow.data;
+    }
+
+     */
+}
