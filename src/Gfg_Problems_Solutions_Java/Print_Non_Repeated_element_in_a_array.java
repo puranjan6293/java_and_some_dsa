02b9e331308723539ebc3eb3dff3b564700939bc
puranjan <mallik.puranjan@gmail.com>
2022-10-05 23:27:43 +0530

    new things added

---
+++ src/Gfg_Problems_Solutions_Java/Print_Non_Repeated_element_in_a_array.java
@@ -0,0 +1,26 @@
+package Gfg_Problems_Solutions_Java;
+
+public class Print_Non_Repeated_element_in_a_array {
+    /*
+    //Function to return non-repeated elements in the array.
+    static ArrayList<Integer> printNonRepeated(int arr[], int n)
+    {
+        // add your code here
+        // Complete the function
+        ArrayList<Integer> list = new ArrayList<>();
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        for(int i=0;i<n;i++){
+            if(hm.containsKey(arr[i])){
+                hm.put(arr[i],hm.get(arr[i])+1);
+            }else{
+                hm.put(arr[i],1);
+            }
+        }
+        for (int i = 0; i < n; i++)
+            if (hm.get(arr[i]) == 1)
+                list.add(arr[i]);
+        return list;
+    }
+
+     */
+}
