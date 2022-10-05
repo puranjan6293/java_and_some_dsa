8bbf14a2263b3b17f613c615963d01b1df2386be
puranjan <mallik.puranjan@gmail.com>
2022-10-05 18:58:48 +0530

    new things added

---
+++ src/Gfg_Problems_Solutions_Java/Nth_Node_from_the_end_of_the_LinkedList.java
@@ -0,0 +1,28 @@
+package Gfg_Problems_Solutions_Java;
+
+public class Nth_Node_from_the_end_of_the_LinkedList {
+    /*
+    //Function to find the data of nth node from the end of a linked list.
+    int getNthFromLast(Node head, int n)
+    {
+        // Your code here
+        int length = 0;
+        Node temp = head;
+        if(head==null){
+            return 0;
+        }
+        while(temp != null) {
+            temp = temp.next;
+            length++;
+        }
+        if(length<n){
+            return -1;
+        }
+        for(int i=0;i<length-n;i++) {
+            head = head.next;
+        }
+        return head.data;
+    }
+
+     */
+}
