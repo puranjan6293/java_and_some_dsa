6b157c7dcd61248534128bf9ec12cbc1273498be
puranjan <mallik.puranjan@gmail.com>
2022-10-07 14:33:21 +0530

    new things added

---
+++ src/Gfg_Problems_Solutions_Java/Detect_loop_in_a_linked_list.java
@@ -0,0 +1,24 @@
+package Gfg_Problems_Solutions_Java;
+
+public class Detect_loop_in_a_linked_list {
+    /*
+    //Function to check if the linked list has a loop.
+    public static boolean detectLoop(Node head){
+        // Add code here
+        Node hare = head;
+        Node turtle = head;
+        if(head==null){
+            return false;
+        }
+        while(hare!=null && hare.next!=null){
+            hare=hare.next.next;
+            turtle=turtle.next;
+            if(hare==turtle){
+                return true;
+            }
+        }
+        return false;
+    }
+
+     */
+}
