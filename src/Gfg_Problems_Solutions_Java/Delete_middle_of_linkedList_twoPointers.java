b8c93cf17cfe998eb7a2c9dff1c63c35364b2b0b
puranjan <mallik.puranjan@gmail.com>
2022-10-07 14:49:30 +0530

    new things added

---
+++ src/Gfg_Problems_Solutions_Java/Delete_middle_of_linkedList_twoPointers.java
@@ -0,0 +1,24 @@
+package Gfg_Problems_Solutions_Java;
+
+public class Delete_middle_of_linkedList_twoPointers {
+    /*
+    Node deleteMid(Node head) {
+        // This is method only submission.
+        // You only need to complete the method.
+        if(head==null || head.next==null){
+            return head;
+        }
+        Node slow = head;
+        Node fast = head;
+        Node curr = null;
+        while(fast!=null && fast.next!=null){
+            curr = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        curr.next = slow.next;
+        return head;
+    }
+
+     */
+}
