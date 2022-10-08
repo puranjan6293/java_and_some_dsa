993b4df4e3d641de22851d60ae90ae00fb318dcd
puranjan <mallik.puranjan@gmail.com>
2022-10-08 15:31:58 +0530

    Longest Sub-Array with Sum K

---
+++ src/Gfg_Problems_Solutions_Java/Key_Pair_Hash.java
@@ -0,0 +1,21 @@
+package Gfg_Problems_Solutions_Java;
+
+import java.util.HashSet;
+
+public class Key_Pair_Hash {
+    boolean hasArrayTwoCandidates(int arr[], int n, int x) {
+        // code here
+        HashSet<Integer> hs = new HashSet<>();
+        for(int i=0;i<n;i++){
+            if(hs.contains(arr[i])){
+                return true;
+            }
+            else{
+                hs.add(x-arr[i]);
+            }
+
+        }
+        return false;
+
+    }
+}
