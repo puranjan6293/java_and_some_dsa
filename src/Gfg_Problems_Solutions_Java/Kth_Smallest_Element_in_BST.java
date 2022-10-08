4f2a4fbca034d401548545940e512864e064517b
puranjan <mallik.puranjan@gmail.com>
2022-10-08 20:24:30 +0530

    new Question added

---
+++ src/Gfg_Problems_Solutions_Java/Kth_Smallest_Element_in_BST.java
@@ -0,0 +1,25 @@
+package Gfg_Problems_Solutions_Java;
+
+public class Kth_Smallest_Element_in_BST {
+    /*
+    static int ans;
+    static int c;
+    public int KthSmallestElement(Node root,int K)
+    {
+        //Your code here
+        ans = -1;
+        c = 0;
+        inorder(root,K);
+        return ans;
+    }
+    public static void inorder(Node root, int K){
+        if(root==null || c>K) return;
+
+        inorder(root.left, K);
+        c++;
+        if(c==K) ans = root.data;
+        inorder(root.right, K);
+    }
+
+     */
+}
