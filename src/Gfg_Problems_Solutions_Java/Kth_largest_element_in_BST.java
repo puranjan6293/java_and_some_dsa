4f3c4275782e9e0e97b7c1f1837cd07e277c802d
puranjan <mallik.puranjan@gmail.com>
2022-10-08 19:42:44 +0530

    delete a node from bst

---
+++ src/Gfg_Problems_Solutions_Java/Kth_largest_element_in_BST.java
@@ -0,0 +1,26 @@
+package Gfg_Problems_Solutions_Java;
+
+public class Kth_largest_element_in_BST {
+    /*
+    // return the Kth largest element in the given BST rooted at 'root'
+    static int ans;
+    static int c;
+    public int kthLargest(Node root,int K)
+    {
+        //Your code here
+        ans = -1;
+        c = 0;
+        inorder(root,K);
+        return ans;
+    }
+    public static void inorder(Node root, int K){
+        if(root==null || c>K) return;
+
+        inorder(root.right, K);
+        c++;
+        if(c==K) ans = root.data;
+        inorder(root.left, K);
+    }
+
+     */
+}
