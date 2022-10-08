f8cb774825a00764aeea09dbe82e5bc6307920bc
puranjan <mallik.puranjan@gmail.com>
2022-10-08 15:33:02 +0530

    Key pair

--- src/Gfg_Problems_Solutions_Java/Key_Pair_Hash.java
+++ src/Gfg_Problems_Solutions_Java/Key_Pair_Hash.java
@@ -3,6 +3,7 @@ package Gfg_Problems_Solutions_Java;
 import java.util.HashSet;
 
 public class Key_Pair_Hash {
+    //y=x-arr[i] formula
     boolean hasArrayTwoCandidates(int arr[], int n, int x) {
         // code here
         HashSet<Integer> hs = new HashSet<>();
