436e499825233552e7eb3dd9867d5cecaeb6493d
puranjan <mallik.puranjan@gmail.com>
2022-10-09 18:03:25 +0530

    new Question added

---
+++ src/Gfg_Problems_Solutions_Java/Merge_two_BST.java
@@ -0,0 +1,30 @@
+package Gfg_Problems_Solutions_Java;
+
+public class Merge_two_BST {
+    /*
+    //Function to return a list of integers denoting the node
+    //values of both the BST in a sorted order.
+    public List<Integer> merge(Node root1,Node root2)
+    {
+        // Write your code here
+        List<Integer> list = new ArrayList<>();
+        traverse(root1, list);
+        traverse(root2, list);
+        Collections.sort(list);
+
+        return list;
+
+    }
+    public static void traverse(Node root, List<Integer> list){
+        if(root==null){
+            return;
+        }
+
+        traverse(root.left, list);
+        list.add(root.data);
+        traverse(root.right, list);
+
+    }
+
+     */
+}
