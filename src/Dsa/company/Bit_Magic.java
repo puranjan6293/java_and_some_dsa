7d70eb1d273836ab1c5db9d31a010a28c8f7d04c
puranjan <mallik.puranjan@gmail.com>
2022-10-09 10:57:35 +0530

    new Question added

---
+++ src/Dsa/company/Bit_Magic.java
@@ -0,0 +1,42 @@
+package Dsa.company;
+import java.util.*;
+public class Bit_Magic {
+    public static void main(String[] args) {
+        /*
+        //Get bit
+        int n = 5;
+        int pos = 2;
+        int bitMask = 1<<pos;
+
+        if((bitMask & n)==0){
+            System.out.println("Bit was zero");
+        }
+        else {
+            System.out.println("Bit was one");
+        }
+
+         */
+        /*
+        //Set bit
+        int n=5;
+        int pos=3;
+        int bitMask=1<<pos;
+
+        int newNumber = bitMask|n;
+        System.out.println(newNumber);
+
+         */
+        /*
+        //Clear bit
+        int n=5;
+        int pos=2;
+        int bitMask=1<<pos;
+        int notBitMask = ~bitMask;
+
+        int newNumber = notBitMask&n;
+        System.out.println(newNumber);
+
+         */
+
+    }
+}
