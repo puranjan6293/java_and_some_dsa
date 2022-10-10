7f014b0bf9af368d4951a54467e200e5aa1ba831
puranjan <mallik.puranjan@gmail.com>
2022-10-10 14:45:37 +0530

    new Question added

---
+++ src/Gfg_Problems_Solutions_Java/Median_of_BST.java
@@ -0,0 +1,30 @@
+package Gfg_Problems_Solutions_Java;
+
+public class Median_of_BST {
+    /*
+    public static float findMedian(Node root)
+    {
+        // code here.
+        ArrayList<Integer> list = new ArrayList<>();
+        inOrder(root, list);
+        int n = list.size();
+
+        if(n%2!=0) return list.get(n/2);
+        else{
+            int back = list.get(n/2);
+            int front = list.get((n/2)-1);
+
+            return (float)(back+front)/2;
+        }
+    }
+    public static void inOrder(Node root, ArrayList<Integer> list){
+        if(root==null){
+            return;
+        }
+        inOrder(root.left, list);
+        list.add(root.data);
+        inOrder(root.right, list);
+    }
+
+     */
+}
