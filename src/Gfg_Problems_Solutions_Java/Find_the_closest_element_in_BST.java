ec67a79b0921617ba0c4f270f95f94f4f78d3689
puranjan <mallik.puranjan@gmail.com>
2022-10-10 15:23:31 +0530

    new Question added

---
+++ src/Gfg_Problems_Solutions_Java/Find_the_closest_element_in_BST.java
@@ -0,0 +1,22 @@
+package Gfg_Problems_Solutions_Java;
+
+public class Find_the_closest_element_in_BST {
+    /*
+    //Function to find the least absolute difference between any node
+    //value of the BST and the given integer.
+    static int minDiff(Node  root, int K)
+    {
+        // Write your code here
+        int min = Integer.MAX_VALUE;
+        while(root!=null){
+            if(root==null) return min;
+            min = Math.min(Math.abs(root.data-K),min);
+
+            if(root.data>K) root = root.left;
+            else root = root.right;
+        }
+        return min;
+    }
+
+     */
+}
