5cdd7da5cc80432dec6406f4b2aa3b72c6fc2f50
puranjan <mallik.puranjan@gmail.com>
2022-10-10 17:16:12 +0530

    new Question added

---
+++ src/Gfg_Problems_Solutions_Java/sentence_into_its_equivalent_num_String.java
@@ -0,0 +1,45 @@
+package Gfg_Problems_Solutions_Java;
+
+import java.util.HashMap;
+
+public class sentence_into_its_equivalent_num_String {
+    String printSequence(String S)
+    {
+        // code here
+        HashMap<Character, String> map = new HashMap<>();
+        map.put('A',"2");
+        map.put('B',"22");
+        map.put('C',"222");
+        map.put('D',"3");
+        map.put('E',"33");
+        map.put('F',"333");
+        map.put('G',"4");
+        map.put('H',"44");
+        map.put('I',"444");
+        map.put('J',"5");
+        map.put('K',"55");
+        map.put('L',"555");
+        map.put('M',"6");
+        map.put('N',"66");
+        map.put('O',"666");
+        map.put('P',"7");
+        map.put('Q',"77");
+        map.put('R',"777");
+        map.put('S',"7777");
+        map.put('T',"8");
+        map.put('U',"88");
+        map.put('V',"888");
+        map.put('W',"9");
+        map.put('X',"99");
+        map.put('Y',"999");
+        map.put('Z',"9999");
+        map.put(' ',"0");
+
+        StringBuilder sb = new StringBuilder();
+        for(char ch:S.toCharArray()){
+            sb.append(map.get(ch));
+        }
+        return sb.toString();
+
+    }
+}
