ce976d47cbd5d581c30eeda877584ad06ce110ae
puranjan <mallik.puranjan@gmail.com>
2022-10-11 19:29:23 +0530

    new Question added

---
+++ src/Gfg_Problems_Solutions_Java/Binary_Tree_to_DLL.java
@@ -0,0 +1,33 @@
+package Gfg_Problems_Solutions_Java;
+
+public class Binary_Tree_to_DLL {
+    /*
+    //step0
+    Node prev = null;
+    Node head = null;
+    //Function to convert binary tree to doubly linked list and return it.
+    Node bToDLL(Node root)
+    {
+        //  Your code here
+        //step1
+        if(root==null) return null;
+
+        //step2
+        bToDLL(root.left);
+
+        //step3
+        if(prev==null) head = root;
+        else{
+            root.left = prev;
+            prev.right = root;
+        }
+        prev = root;
+
+        //step4
+        bToDLL(root.right);
+
+        return head;
+    }
+
+     */
+}
