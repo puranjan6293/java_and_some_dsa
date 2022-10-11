71812b1e920abdd40e15b9133a49baf02fab3743
puranjan <mallik.puranjan@gmail.com>
2022-10-11 14:08:30 +0530

    new Question added

---
+++ src/Gfg_Problems_Solutions_Java/Implement_Stack_using_LinkedList.java
@@ -0,0 +1,45 @@
+package Gfg_Problems_Solutions_Java;
+/*
+class MyStack
+{
+    // class StackNode {
+    //     int data;
+    //     StackNode next;
+    //     StackNode(int a) {
+    //         data = a;
+    //         next = null;
+    //     }
+    // }
+    StackNode top = null;
+    StackNode tail = null;
+    int size = 0;
+
+    //Function to push an integer into the stack.
+    void push(int a)
+    {
+        // Add your code here
+        StackNode newNode = new StackNode(a);
+        newNode.next = top;
+        top = newNode;
+        size++;
+    }
+
+    //Function to remove an item from top of the stack.
+    int pop()
+    {
+        // Add your code here
+        if(top==null){
+            return -1;
+        }
+        int x = top.data;
+        top = top.next;
+        size--;
+        return x;
+    }
+}
+
+ */
+
+public class Implement_Stack_using_LinkedList {
+
+}
