48e2fdb369fb9af8b93d5066a0bd4947867e57b6
puranjan <mallik.puranjan@gmail.com>
2022-10-15 14:38:49 +0530

    new Question added

---
+++ src/Gfg_Problems_Solutions_Java/Longest_Common_Substring_DynamicProg.java
@@ -0,0 +1,24 @@
+package Gfg_Problems_Solutions_Java;
+
+public class Longest_Common_Substring_DynamicProg {
+    int longestCommonSubstr(String S1, String S2, int n, int m){
+        // code here
+        int[][] dp = new int[n+1][m+1];
+        int ans = 0;
+
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=m;j++){
+                if(i==0 || j==0){
+                    dp[i][j] = 0;
+                }
+                else if(S1.charAt(i-1)==S2.charAt(j-1)){
+                    dp[i][j] = 1+dp[i-1][j-1];
+                    ans = Math.max(ans, dp[i][j]);
+                }
+                else dp[i][j]=0;
+            }
+        }
+        return ans;
+
+    }
+}
