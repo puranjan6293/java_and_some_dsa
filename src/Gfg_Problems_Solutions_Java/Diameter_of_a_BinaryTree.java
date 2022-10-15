58cb396ceca4f76def83e853fd05749d58291ecc
puranjan <mallik.puranjan@gmail.com>
2022-10-15 18:37:17 +0530

    new Question added

---
+++ src/Gfg_Problems_Solutions_Java/Diameter_of_a_BinaryTree.java
@@ -0,0 +1,23 @@
+package Gfg_Problems_Solutions_Java;
+
+public class Diameter_of_a_BinaryTree {
+    /*
+    int ans = 0;
+    // Function to return the diameter of a Binary Tree.
+    int diameter(Node root) {
+        // Your code here
+        dia(root);
+        return ans;
+    }
+    int dia(Node root){
+        if(root==null) return 0;
+
+        int lft = dia(root.left);
+        int rit = dia(root.right);
+
+        ans = Math.max(ans, lft+rit+1);
+        return 1+Math.max(lft,rit);
+    }
+
+     */
+}
