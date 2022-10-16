1bc232b6e05fbd8d5e4d8ff491d49d6823e56033
puranjan <mallik.puranjan@gmail.com>
2022-10-17 00:06:15 +0530

    new Question added

---
+++ src/LeetCode_solution/Remove_Element__Array.java
@@ -0,0 +1,14 @@
+package LeetCode_solution;
+
+public class Remove_Element__Array {
+    public int removeElement(int[] nums, int val) {
+        int count = 0;
+        for(int i=0;i<nums.length;i++){
+            nums[i-count] = nums[i];
+            if(nums[i]==val){
+                count++;
+            }
+        }
+        return nums.length-count;
+    }
+}
