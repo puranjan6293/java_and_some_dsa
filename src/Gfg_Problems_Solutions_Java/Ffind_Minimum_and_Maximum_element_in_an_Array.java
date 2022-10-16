bcdffa0bc9019810b00d051807fb6952b33ae431
puranjan <mallik.puranjan@gmail.com>
2022-10-16 14:51:08 +0530

    new Question added

---
+++ src/Gfg_Problems_Solutions_Java/Ffind_Minimum_and_Maximum_element_in_an_Array.java
@@ -0,0 +1,28 @@
+package Gfg_Problems_Solutions_Java;
+
+public class Ffind_Minimum_and_Maximum_element_in_an_Array {
+    /*
+    static pair getMinMax(long a[], long n)
+    {
+        //Write your code here
+        //approach1
+        long min;
+        long max;
+        Arrays.sort(a);
+        min = a[0];
+        max = a[a.length-1];
+        return new pair(min, max);
+
+        //approach2
+        long min=a[0];
+        long max=a[0];
+
+        for(int i=0;i<n;i++){
+            if(a[i]<min) min = a[i];
+            if(a[i]>max) max = a[i];
+        }
+        return new pair(min, max);
+    }
+
+     */
+}
