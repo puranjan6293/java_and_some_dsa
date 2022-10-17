1aaae6dd1a4e9448fb4e37c5c919f34cb622dcfa
puranjan <mallik.puranjan@gmail.com>
2022-10-17 12:31:35 +0530

    new Question added

---
+++ src/LeetCode_solution/Range_Sum_of__BST.java
@@ -0,0 +1,19 @@
+package LeetCode_solution;
+
+public class Range_Sum_of__BST {
+    /*
+    int sum = 0;
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        if(root==null) return 0;
+        if(root.val<low) return rangeSumBST(root.right, low, high);
+        if(root.val>high) return rangeSumBST(root.left, low, high);
+        else{
+            rangeSumBST(root.left, low, high);
+            sum+=root.val;
+            rangeSumBST(root.right, low, high);
+        }
+        return sum;
+    }
+
+     */
+}
