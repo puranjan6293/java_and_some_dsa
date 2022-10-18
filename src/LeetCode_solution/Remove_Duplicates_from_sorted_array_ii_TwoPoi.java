4811df70a0c2a28bc8a2c5ef9ed5213c01abbcc3
puranjan <mallik.puranjan@gmail.com>
2022-10-18 14:40:00 +0530

    new Question added

---
+++ src/LeetCode_solution/Remove_Duplicates_from_sorted_array_ii_TwoPoi.java
@@ -0,0 +1,15 @@
+package LeetCode_solution;
+
+public class Remove_Duplicates_from_sorted_array_ii_TwoPoi {
+    public int removeDuplicates(int[] nums) {
+        int n = nums.length;
+        if(n<3) return n;
+        int i = 2;
+        for(int j=i;j<n;j++){
+            if(nums[j]!=nums[i-2]){
+                nums[i++] = nums[j];
+            }
+        }
+        return i;
+    }
+}
