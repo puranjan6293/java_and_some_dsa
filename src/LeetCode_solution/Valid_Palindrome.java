4f4c79da715be430aab8b140e10b69de77d8488d
puranjan <mallik.puranjan@gmail.com>
2022-10-22 16:50:54 +0530

    new Question added

---
+++ src/LeetCode_solution/Valid_Palindrome.java
@@ -0,0 +1,26 @@
+package LeetCode_solution;
+
+public class Valid_Palindrome {
+    public boolean isPalindrome(String s) {
+        if(s==null) return false;
+
+        int left = 0;
+        int right = s.length()-1;
+
+        while(left<right){
+
+            while(left<right && !Character.isLetterOrDigit(s.charAt(left))) left++;
+
+            while(left<right && !Character.isLetterOrDigit(s.charAt(right))) right--;
+
+            if(Character.toLowerCase(s.charAt(left)) !=
+                    Character.toLowerCase(s.charAt(right))){
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
