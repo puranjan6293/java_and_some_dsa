da83e8158abee5b67339ceb69723d861ebc8ea47
puranjan <mallik.puranjan@gmail.com>
2022-10-22 10:35:12 +0530

    new Question added

---
+++ src/LeetCode_solution/InOrder_Traversal.java
@@ -0,0 +1,16 @@
+package LeetCode_solution;
+
+public class InOrder_Traversal {
+    /*
+    List<Integer> ans = new ArrayList<>();
+    public List<Integer> inorderTraversal(TreeNode root) {
+
+        if(root==null) return ans;
+        inorderTraversal(root.left);
+        ans.add(root.val);
+        inorderTraversal(root.right);
+        return ans;
+    }
+
+     */
+}
