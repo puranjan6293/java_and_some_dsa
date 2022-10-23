6f3a315bacbf91f3370490a55e2744b98cd75984
puranjan <mallik.puranjan@gmail.com>
2022-10-23 11:19:32 +0530

    new Question added

---
+++ src/LeetCode_solution/Palindrome_LinkedList.java
@@ -0,0 +1,22 @@
+package LeetCode_solution;
+
+public class Palindrome_LinkedList {
+    /*
+    public boolean isPalindrome(ListNode head) {
+        ListNode temp = head;
+        Stack stack = new Stack();
+        while(temp!=null){
+            stack.push(temp.val);
+            temp = temp.next;
+        }
+        while(head!=null){
+            if(head.val!=(int)stack.pop()){
+                return false;
+            }
+            head = head.next;
+        }
+        return true;
+    }
+
+     */
+}
